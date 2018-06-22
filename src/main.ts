f4baf1f24465b5e78cadd891fbfe5eaa33c8bce2
diegodoradofernandez <40492943+diegodoradofernandez@users.noreply.github.com>
2018-06-22 14:29:39 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,13 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  //https://unsplash.com/
+
+    //accesskey
+    //39488dda8af85a2bb0582f78265b446d49e13ab8e4d047e0a21f9f9bba44079e
+
+    //Secret key
+    //fd4308d80a038dc5fb5ccf66a6e9f93d276d71271ea3056260df9ce29b71959d
+
+ 

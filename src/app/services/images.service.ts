661f58a9f50555cbb7188b9215a562c04621e653
diegodoradofernandez <40492943+diegodoradofernandez@users.noreply.github.com>
2018-06-23 12:12:47 +0200

    Add files via upload

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -1,3 +1,75 @@
+<<<<<<< HEAD
+
+import {Injectable, EventEmitter} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import {environment} from '../../environments/environment';
+
+@Injectable()
+export class ImagesService {
+
+  private url = 'https://api.unsplash.com';  // URL web API
+  private applicationId = environment.applicationId;
+  searchEvent: EventEmitter<any> = new EventEmitter();
+  page = 1;
+  per_page = 10;
+  cache = {};
+
+  constructor(private http: Http) {
+  }
+
+
+  /**
+   * Mensaje de error.
+   * @param res
+   * @returns {any|Array}
+   */
+  handleError(error: Response | any) {
+    let errorMessage: string;
+    if (error.status === 403 && error._body === 'Rate Limit Exceeded') {
+      errorMessage = 'Se ha superado el límite diario. Por favor, inténtalo más tarde';
+    } else {
+      errorMessage = 'Ha ocurrido un error, inténtalo más tarde.';
+    }
+    return Observable.throw(errorMessage);
+  }
+
+
+  /**
+   * Busqueda de imágenes
+   * @param query
+   * @param page
+   */
+  search(query: string, page: number) {
+    if (query.length === 0) {
+      this.searchEvent.emit('clear');
+      return;
+    }
+
+    this.searchEvent.emit({loading: true, page : page, query : query});
+    let url = `${this.url}/search/photos?client_id=${this.applicationId}`;
+    url += `&page=${page}&per_page=${this.per_page}&query=${query}`;
+
+    this.http.get(url)
+      .map(response => response.json())
+      .subscribe(images => {
+        this.searchEvent.emit(images);
+      }, this.handleError);
+  }
+
+
+  /**
+   * Listener de busqueda
+   * @returns {EventEmitter<any>}
+   */
+  getSearchEvent() {
+    return this.searchEvent;
+  }
+
+=======
 
 import {Injectable, EventEmitter} from '@angular/core';
 import {Http, Response} from '@angular/http';
@@ -92,4 +164,5 @@ export class ImagesService {
       .catch(this.handleError);
   }
 
+>>>>>>> e2cca6f72741cc61600428950af2ff55af5ddba8
 }
--- src/app/services/numeros.service.ts
+++ src/app/services/numeros.service.ts
@@ -1,3 +1,60 @@
+<<<<<<< HEAD
+
+
+
+import {Injectable, EventEmitter} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
+
+@Injectable()
+export class NumerosService {
+
+  private url = 'https://qrng.anu.edu.au/API/jsonI.php?length=7&type=';  // URL
+
+  private _uint16 = "uint16";
+
+  private _uint8 = "uint8";
+
+  constructor(private http: Http) {
+  }
+
+
+  /**
+   * Obtenemos un array de numeros aleatorios.
+   * @returns {Observable<R|T>}
+   */
+  random16(): Observable<any[]> {
+    return this.http.get(this.url + this._uint16)
+      .map(response => response.json())
+      .catch(this.handleError);
+  }
+
+  random8(): Observable<any[]> {
+    return this.http.get(this.url + this._uint8)
+      .map(response => response.json())
+      .catch(this.handleError);
+  }
+
+
+  /**
+   * Error genérico
+   * @param res
+   * @returns {any|Array}
+   */
+  handleError(error: Response | any) {
+    let errorMessage: string;
+      errorMessage = 'Error al obtener los números';
+    return Observable.throw(errorMessage);
+  }
+
+
+  
+
+=======
 
 
 
@@ -44,4 +101,5 @@ export class NumerosService {
 
   
 
+>>>>>>> e2cca6f72741cc61600428950af2ff55af5ddba8
 }

37261055670891012077e476ea4835126d8facac
diegodoradofernandez <40492943+diegodoradofernandez@users.noreply.github.com>
2018-06-23 12:15:11 +0200

    Add files via upload

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+<<<<<<< HEAD
+export class AboutComponent {
+
+  constructor() { }
+
+=======
 export class AboutComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+>>>>>>> e2cca6f72741cc61600428950af2ff55af5ddba8
 
 }

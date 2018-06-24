e7f41c25764866330da300b8efdbd538f79f759c
Diego Dorado Fernández <diego.dorado@nomatch.es>
2018-06-24 10:01:49 +0200

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  applicationId : '39488dda8af85a2bb0582f78265b446d49e13ab8e4d047e0a21f9f9bba44079e',
-  apiKeyRandom:  '192913c3-d9a3-4876-a1ca-e3e6bf415d11'
+  applicationId : '39488dda8af85a2bb0582f78265b446d49e13ab8e4d047e0a21f9f9bba44079e'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  applicationId : '39488dda8af85a2bb0582f78265b446d49e13ab8e4d047e0a21f9f9bba44079e',
-  apiKeyRandom:  '192913c3-d9a3-4876-a1ca-e3e6bf415d11'
+  applicationId : '39488dda8af85a2bb0582f78265b446d49e13ab8e4d047e0a21f9f9bba44079e'
 };
 
 /*
